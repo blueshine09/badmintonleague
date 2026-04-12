9fea0b585a3a0c43707385116434d45d56a14796
박진영 <blueshine7223@gmail.com>
2026-04-12 19:41:53 +0900

    first upload

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+window.DEFAULT_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyATMyJPZrX4dYMB7_2RLIBBO_iZvQOi0jI",
+  authDomain: "badminton-e0486.firebaseapp.com",
+  databaseURL: "https://badminton-e0486-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "badminton-e0486",
+  storageBucket: "badminton-e0486.firebasestorage.app",
+  messagingSenderId: "438005497656",
+  appId: "1:438005497656:web:c4ff4ee"
+};
